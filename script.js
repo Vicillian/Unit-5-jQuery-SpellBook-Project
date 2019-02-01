6b4f608375ae16531e5eadaf1e78c485c78fb617
Vicillian <44278914+Vicillian@users.noreply.github.com>
2019-01-31 16:48:24 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$(".fire").click(function(){
+    $("#ball").fadeToggle();
+});
+
+
+
+//Spell #2
+
+$("water").click(function(){
+    $("ballw").fadeToggle();
+});
+
+
+
+//Spell #3
+
+$("      ").click(function(){
+ 
+});
+
+
+
